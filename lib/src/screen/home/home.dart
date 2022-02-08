0dba23808f1bb3a33d6c66e0dbb9e92d77585dd3
coder-Aayush <bhattaraiaayush08@gmail.com>
2022-02-08 23:10:19 +0545

    refactor into statelesswidget

--- lib/src/screen/home/home.dart
+++ lib/src/screen/home/home.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hospital/src/models/hosptial.models.dart';
 import 'package:hospital/src/provider/hospital.provider.dart';
 import 'package:hospital/src/screen/home/widgets/body_painter.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final _hospitalState = ref.watch(hospitalProvider);
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           leading: const BackButton(),
@@ -36,7 +36,28 @@ class HomeView extends ConsumerWidget {
                     ],
                   ),
                 ),
-               
+                Consumer(
+                  builder: (context, ref, child) {
+                    final _hospitalState = ref.watch(hospitalProvider);
+                    if (_hospitalState.hospitals == null) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (_hospitalState.hospitals!.length == 0) {
+                      return const Center(child: Text("No Ambulance Near You"));
+                    } else {
+                      return Flexible(
+                        child: ListView.builder(
+                          itemBuilder: (context, index) {
+                            Ambulance ambulance =
+                                _hospitalState.hospitals![index];
+                            return ListTile(
+                              title: Text(ambulance.title ?? ""),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  },
+                )
               ],
             ),
             CustomPaint(
